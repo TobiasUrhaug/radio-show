ebb6c3a80bc671ffffa0681d3855cbbdc8c7b604
Tobias Urhaug <tobiasurhaug@gmail.com>
2020-04-03 17:34:23 +0200

    Bootstrap spring kotlin project

---
+++ src/main/kotlin/no/omtheorem/radio/RadioApplication.kt
@@ -0,0 +1,11 @@
+package no.omtheorem.radio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RadioApplication
+
+fun main(args: Array<String>) {
+	runApplication<RadioApplication>(*args)
+}
---
+++ src/test/kotlin/no/omtheorem/radio/RadioApplicationTests.kt
@@ -0,0 +1,13 @@
+package no.omtheorem.radio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RadioApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
