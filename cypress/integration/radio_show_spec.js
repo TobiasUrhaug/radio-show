a33e822072517313556966c3a474d839876f6202
Tobias Urhaug <tobiasurhaug@gmail.com>
2020-04-06 15:42:00 +0200

    Move test to radio shows block

--- cypress/integration/radio_show_spec.js
+++ cypress/integration/radio_show_spec.js
@@ -5,13 +5,17 @@ describe('Home page', function() {
     cy.contains('Welcome')
   })
 
-  it('Contains a list of previous shows', function() {
-    cy.visit('http://localhost:8080')
+  describe('Radio shows', function() {
 
-    var radioShows = cy.get('#radio-shows')
-    radioShows.find('h2').first().should('contain', 'Radio shows')
-    radioShows.get('ul>li').eq(0).should('contain', 'Transmission#1')
-    radioShows.get('ul>li').eq(1).should('contain', 'Transmission#2')
+    it('Contains a list of previous shows', function() {
+      cy.visit('http://localhost:8080')
+
+      var radioShows = cy.get('#radio-shows')
+      radioShows.find('h2').first().should('contain', 'Radio shows')
+      radioShows.get('ul>li').eq(0).should('contain', 'Transmission#1')
+      radioShows.get('ul>li').eq(1).should('contain', 'Transmission#2')
+    })
+
   })
 
 })
