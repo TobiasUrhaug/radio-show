f6cb2caf4512508e67bc9b6920f3d870ac1dee2a
Tobias Urhaug <tobiasurhaug@gmail.com>
2020-04-06 12:26:32 +0200

    Add list of previous shows to homepage

--- cypress/integration/radio_show_spec.js
+++ cypress/integration/radio_show_spec.js
@@ -1,7 +1,18 @@
 describe('Home page', function() {
+
   it('Displays welcome message', function() {
     cy.visit('http://localhost:8080')
     cy.contains('Welcome')
   })
+
+  it('Contains a list of previous shows', function() {
+    cy.visit('http://localhost:8080')
+
+    var radioShows = cy.get('#radio-shows')
+    radioShows.find('h2').first().should('contain', 'Radio shows')
+    radioShows.get('ul>li').eq(0).should('contain', 'Transmission#1')
+    radioShows.get('ul>li').eq(1).should('contain', 'Transmission#2')
+  })
+
 })
 
