3981546f0e0528c647444e21b58de34c87caf6ae
Tobias Urhaug <tobiasurhaug@gmail.com>
2020-04-11 12:26:52 +0200

    Refactor tests to visit baseurl before each

--- cypress/integration/radio_show_spec.js
+++ cypress/integration/radio_show_spec.js
@@ -1,15 +1,16 @@
 describe('Home page', function() {
 
-  it('Displays welcome message', function() {
+  beforeEach(() => {
     cy.visit('/')
+  })
+
+  it('Displays welcome message', function() {
     cy.contains('Welcome')
   })
 
   describe('Radio shows', function() {
 
     it('Contains a list of previous shows', function() {
-      cy.visit('/')
-
       var radioShows = cy.get('#radio-shows')
       radioShows.find('h2').first().should('contain', 'Radio shows')
       radioShows.get('ul>li').eq(0).should('contain', 'Transmission#1')
@@ -17,7 +18,6 @@ describe('Home page', function() {
     })
 
     it('Add new show button redirects to add show page', function() {
-        cy.visit('/')
         cy.get('#radio-shows').contains('Create new show').click()
         cy.url().should('contain', '/shows/add')
     })
