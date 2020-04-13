7d5e65827ad9a28019ec27754f8ca8aed13ca35a
Tobias Urhaug <tobiasurhaug@gmail.com>
2020-04-13 11:28:10 +0200

    Extract helper method for adding shows

--- cypress/integration/radio_show_spec.js
+++ cypress/integration/radio_show_spec.js
@@ -33,20 +33,24 @@ describe('Home page', function() {
     })
 
     it('Lets users add a new show', () => {
-      cy.get('[data-cy=add-show]').click()
-      cy.get('[data-cy=name-input').type('A new show!')
-      cy.get('[data-cy=date-input').type('2020-04-10')
-      cy.get('[data-cy=submit').click()
+      add_show('A new Show!', '2020-04-10')
       cy.url().should('eq', 'http://localhost:8080/')
 
       cy.get('td>span')
         .eq(0)
-        .should('contain', 'A new show!')
+        .should('contain', 'A new Show!')
 
       cy.get('td>span')
         .eq(1)
         .should('contain', '2020-04-10')
     })
+
+    function add_show(name, date) {
+      cy.get('[data-cy=add-show]').click()
+      cy.get('[data-cy=name-input').type(name)
+      cy.get('[data-cy=date-input').type(date)
+      cy.get('[data-cy=submit').click()
+    }
 
   })
 
