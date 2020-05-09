a5cd03e7cc48776cf14a3beca6b980b6f8660a6a
Tobias Urhaug <tobiasurhaug@gmail.com>
2020-05-09 14:30:14 +0200

    Add jasmine spec for tracklist.js

---
+++ jasmine/spec/TracklistSpec.js
@@ -0,0 +1,48 @@
+describe("Tracklist", function() {
+
+  it("addTrack adds a track to the tracklist", function() {
+    loadFixtures('tracklist.html');
+
+    const track = {artist: 'Artist Name', title: 'track title', url: 'url'};
+    $('#artist-input').val(track.artist);
+    $('#title-input').val(track.title);
+    $('#url-input').val(track.url);
+
+    addTrack();
+
+    let addedTrack = $('#tracklist tr').eq(0);
+    expect($(addedTrack.find('#artist')).val()).toBe(track.artist);
+    expect($(addedTrack.find('#title')).val()).toBe(track.title);
+    expect($(addedTrack.find('#url')).val()).toBe(track.url);
+  });
+
+  it("addTrack adds the track to the second last row, above the input fields", function() {
+    loadFixtures('tracklist.html');
+
+    const track = {artist: 'Artist Name', title: 'track title', url: 'url'};
+    $('#artist-input').val(track.artist);
+    $('#title-input').val(track.title);
+    $('#url-input').val(track.url);
+
+    addTrack();
+
+    expect($('#tracklist tr').eq(1).attr('id')).toBe('inputs');
+  });
+
+  it("addTrack clears input fields after adding a track", function() {
+    loadFixtures('tracklist.html');
+
+    const track = {artist: 'Artist Name', title: 'track title', url: 'url'};
+    $('#artist-input').val(track.artist);
+    $('#title-input').val(track.title);
+    $('#url-input').val(track.url);
+
+    addTrack();
+
+    expect($('#artist-input').val()).toBe('');
+    expect($('#title-input').val()).toBe('');
+    expect($('#url-input').val()).toBe('');
+  });
+
+});
+
--- src/main/resources/static/js/tracklist.js
+++ src/main/resources/static/js/tracklist.js
@@ -8,10 +8,10 @@ function addTrack() {
   $('#url-input').val('');
 
   let rowNumber = $('#tracklist tr').length - 1;
-  let artistCell = '<td><input name="tracks[' + rowNumber + '].artist" type="text" value="' + artist + '" data-test="artist" readonly/></td>'
-  let titleCell = '<td><input name="tracks[' + rowNumber + '].title" type="text" value="' + title + '" data-test="title" readonly/></td>'
-  let urlCell = '<td><input name="tracks[' + rowNumber + '].url" type="text" value="' + url + '" data-test="url" readonly/></td>'
+  let artistCell = '<td><input name="tracks[' + rowNumber + '].artist" id="artist" type="text" value="' + artist + '" data-test="artist" readonly/></td>'
+  let titleCell = '<td><input name="tracks[' + rowNumber + '].title" id="title" type="text" value="' + title + '" data-test="title" readonly/></td>'
+  let urlCell = '<td><input name="tracks[' + rowNumber + '].url" id="url" type="text" value="' + url + '" data-test="url" readonly/></td>'
 
-  let row = '<tr data-test="tracks">' + artistCell + titleCell + urlCell + '</tr>';
+  let row = '<tr id="tracks" data-test="tracks">' + artistCell + titleCell + urlCell + '</tr>';
   $(row).insertBefore('#inputs');
 }
